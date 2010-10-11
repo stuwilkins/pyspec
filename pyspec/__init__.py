d90a8caf32e2d3078db02de93f0c72344fa40b4a
stuwilkins <stuwilkins@d19dc245-ee4a-0410-ba2b-bd697e8c3f69>
2010-10-11 16:32:09 +0000

    Deleted old and new mpfit routines Added c routines for levmar integration
    git-svn-id: http://svn.code.sf.net/p/pyspec/code/trunk@32 d19dc245-ee4a-0410-ba2b-bd697e8c3f69

--- pyspec/__init__.py
+++ pyspec/__init__.py
@@ -1,8 +1,3 @@
-# Enforce proper version requirements
-import sys
-from spec import SpecDataFile
-from spec import SpecScan
 
-if sys.version[0:3] < '2.5':
-    raise ImportError('Python Version 2.5 or above is required for pyspec.')
+
 
