e339b65d82cd01999b6c7bc6d83671161db50e68
stuwilkins <stuwilkins@d19dc245-ee4a-0410-ba2b-bd697e8c3f69>
2010-10-13 23:27:26 +0000

    Modified setup.py for linux
    git-svn-id: http://svn.code.sf.net/p/pyspec/code/trunk@43 d19dc245-ee4a-0410-ba2b-bd697e8c3f69

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 from distutils.core import setup, Extension
 import numpy as np
 
-LEVMAR_LIBDIR = '/Users/swilkins/Dropbox/Programming/Python/levmar-2.5'
-LEVMAR_INCDIR = '/Users/swilkins/Dropbox/Programming/Python/levmar-2.5'
+#LEVMAR_LIBDIR = '/Users/swilkins/Dropbox/Programming/Python/levmar-2.5'
+#LEVMAR_INCDIR = '/Users/swilkins/Dropbox/Programming/Python/levmar-2.5'
+LEVMAR_LIBDIR = '/home/tardis/swilkins/C-C++/levmar-2.5'
+LEVMAR_INCDIR = '/home/tardis/swilkins/C-C++/levmar-2.5'
 
 levmar = Extension('pylevmar', ['src/pylevmar.c'],
-                   libraries = ['levmar', 'm'],
+                   libraries = ['levmar', 'm', 'blas', 'lapack'],
                    extra_compile_args = ['-g'],
                    library_dirs = ['/usr/lib', LEVMAR_LIBDIR],
                    include_dirs = [LEVMAR_INCDIR, np.get_include()],
