9294350c7d9fa4877fd25efe1e8543bfce1f3def
stuwilkins <stuwilkins@d19dc245-ee4a-0410-ba2b-bd697e8c3f69>
2010-10-16 17:41:10 +0000

    Added config for linux. Rebuilt setup to include levmar
    git-svn-id: http://svn.code.sf.net/p/pyspec/code/trunk@47 d19dc245-ee4a-0410-ba2b-bd697e8c3f69

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
-from setupext import options
+from setupext import options, levmar
 import numpy as np
 
-LEVMAR_LIBDIR = '/Users/swilkins/Dropbox/Programming/Python/levmar-2.5'
-LEVMAR_INCDIR = '/Users/swilkins/Dropbox/Programming/Python/levmar-2.5'
+#LEVMAR_LIBDIR = '/Users/swilkins/Dropbox/Programming/Python/levmar-2.5'
+#LEVMAR_INCDIR = '/Users/swilkins/Dropbox/Programming/Python/levmar-2.5'
 #LEVMAR_LIBDIR = '/home/tardis/swilkins/C-C++/levmar-2.5'
 #LEVMAR_INCDIR = '/home/tardis/swilkins/C-C++/levmar-2.5'
-LIBLINUX = ['levmar', 'm', 'blas', 'lapack']
-LIBMACOSX = ['levmar', 'm']
+#LIBLINUX = ['levmar', 'm', 'blas', 'lapack']
+#LIBMACOSX = ['levmar', 'm']
 
 ext_modules = []
 if options['build_levmar']:
     ext_modules.append(Extension('pyspec.pylevmar', ['src/pylevmar.c'],
-                                 libraries = LIBMACOSX,
+                                 libraries = levmar['libraries'],
                                  extra_compile_args = ['-g'],
-                                 library_dirs = ['/usr/lib', LEVMAR_LIBDIR],
-                                 include_dirs = [LEVMAR_INCDIR, np.get_include()],
+                                 library_dirs = levmar.library_dirs,
+                                 include_dirs = levmar.include_dirs,
                                  depends = ['src/pylevmar.h']))
 
 setup(name='pyspec',
--- setupext.py
+++ setupext.py
@@ -1,12 +1,31 @@
 
 import os
 import ConfigParser
+import numpy as np
 
 options = {'build_levmar' : False }
+levmar  = {'include_dirs' : [],
+           'library_dirs' : [],
+           'libraries'    : [] }
 
+def parseExtensionSetup(name, config, default):
+    
+    try: default['include_dirs'] = config.get(name, "include_dirs").split(os.pathsep)
+    except: pass
+    try: default['library_dirs'] = config.get(name, "library_dirs").split(os.pathsep)
+    except: pass
+    try: default['libraries'] = config.get(name, "libraries").split(",")
+    except: pass
+
+    return default
+    
 if os.path.exists("setup.cfg"):
     config = ConfigParser.SafeConfigParser()
     config.read("setup.cfg")
 
-    try: options['build_levmar'] = config.getboolean("levmar","build_levmar")
+    try: options['build_levmar'] = config.getboolean("levmar","build")
     except: pass
+
+    levmar = parseExtensionSetup('levmar', config, levmar)
+    levmar['include_dirs'].append(np.get_include())
+
